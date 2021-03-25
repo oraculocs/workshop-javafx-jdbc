e902840f2211630d2c9a3904f07bb75279f61e1b
Carlos Augusto <augustino2003@hotmail.com>
2021-03-25 17:13:25 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,30 +1,26 @@
 package application;
-<<<<<<< HEAD
 
 import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
-=======
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
->>>>>>> 0f55e3c4c544726a3fd0b02c0cb1bae62cd70218
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-<<<<<<< HEAD
+
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -33,18 +29,7 @@ public class Main extends Application {
 		}
 	}
 
-=======
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
->>>>>>> 0f55e3c4c544726a3fd0b02c0cb1bae62cd70218
+
 	public static void main(String[] args) {
 		launch(args);
 	}
